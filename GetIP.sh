c5cfd08ab0f0242fef74612b98ca11e68b73764b
gyogyika <info@gyogyi.info>
2022-08-07 15:46:15 +0200

    curl timeout added

--- GetIP.sh
+++ GetIP.sh
@@ -5,13 +5,13 @@ source /root/settings.ini
 echo
 URL="$GETIP_URL"?name="$NAME"
 echo "GetIP:" "$URL"
-curl --max-time 5 "$URL"
+curl --max-time $CURL_TIMEOUT "$URL"
 echo
 
 if [ -n "$FREEMYIP" ]
 then
   echo "freemyip:" "$FREEMYIP"
-  curl --max-time 5 "$FREEMYIP"
+  curl --max-time $CURL_TIMEOUT "$FREEMYIP"
   echo
 fi
 
